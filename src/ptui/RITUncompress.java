f504032a9d5565282fa29d44753014e724ceef84
Cody Smith <bcs4313@g.rit.edu>
2020-11-21 17:41:49 -0500

    (Cody & Dylan) RITUncompress Builds a list of values from the given compressed text file.

--- src/ptui/RITUncompress.java
+++ src/ptui/RITUncompress.java
@@ -1,10 +1,46 @@
 package ptui;
 
+import model.RITQTNode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class RITUncompress {
+    private static String arguments[];
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java RITUncompress compressed.rit uncompressed.txt");
             return;
         }
+        arguments = args;
+        File compressedImage = new File("compressed\\" + args[0]);
+        RITUncompress decompressor = new RITUncompress();
+        decompressor.buildList(compressedImage);
+    }
+
+    public ArrayList<Integer> buildList(File compressedImg)
+    {
+        Scanner scnr = null;
+        try
+        {
+            scnr = new Scanner(compressedImg);
+        }
+        catch(FileNotFoundException fnfe)
+        {
+            System.err.println("Error: The file name specified in argument 0 is invalid: " + arguments[0]);
+            System.exit(0);
+        }
+
+        ArrayList<Integer> compressedValues = new ArrayList<Integer>();
+
+        while(scnr.hasNextLine())
+        {
+            int currentVal = Integer.parseInt(scnr.nextLine());
+            compressedValues.add(currentVal);
+        }
+        return compressedValues;
     }
 }
