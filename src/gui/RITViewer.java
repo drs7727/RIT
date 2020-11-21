68fe19997e74f9602268e34caf3be75ac605c3a4
Dylan <drs7727@g.rit.edu>
2020-11-21 17:16:52 -0500

    Dylan and Cody: handles all errors

--- src/gui/RITViewer.java
+++ src/gui/RITViewer.java
@@ -11,26 +11,54 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * RITViewer takes an uncompressed image full of grayscale
+ * pixel values and puts it on display using JavaFX.
+ *
+ * Two arguments are put in, the name of the uncompressed file and the base width/height.
+ * The base width and height have to be equal for the program to function.
+ *
+ * @author Cody Smith (bcs4313@g.rit.edu)
+ * @author Dylan Spence (drs7727@g.rit.edu)
+ */
 public class RITViewer extends Application {
 
     // stores the arguments from the main method
     private static String[] arguments;
 
+    /**
+     * Starting point of the application.
+     * Generates an image and displays it.
+     * @param stage the stage (window) the application is using
+     * @throws Exception exception errors usually occur from invalid images / decompressed files
+     */
     @Override
-    public void start(Stage stage) throws Exception {
-        if(arguments.length != 2)
-        {
+    public void start(Stage stage) throws Exception{
+        if(arguments.length != 2) {
             System.out.println("Usage: filename img_width/height");
             System.exit(0);
         }
 
-        File image = new File("uncompressed\\" + arguments[0]);
-        Scanner scnr = new Scanner(image);
+        File testImage;
+        Scanner scnr = null;
+
+        // create file scanner with error handling
+        try {
+            testImage = new File("uncompressed\\" + arguments[0]);
+            scnr = new Scanner(testImage);
+        }
+        catch(IOException fnfe) {
+            System.err.println("Error: The file name specified in argument 0 is invalid: " + arguments[0]);
+            System.exit(0);
+        }
 
         // base resolution width and height for image
         int res_base = Integer.parseInt(arguments[1]);
+
 
         // location tracker for width --> allows us to know when to change the y value
         int x = 0;
@@ -38,16 +66,29 @@ public class RITViewer extends Application {
         // location tracker for height
         int y = 0;
 
+        // Create group structure for image display
         Group g = new Group();
-        Canvas can = new Canvas(res_base, res_base);
+        Canvas can = new Canvas(res_base-1, res_base-1);
         GraphicsContext gc = can.getGraphicsContext2D();
 
+        // Scanner loop to find all integer values for grayscale
         while(scnr.hasNextLine())
         {
             String line = scnr.nextLine();
-            if(x<res_base-1)
-            {
-                double val = Double.parseDouble(line);
+            if(x<res_base-1) {
+                double val = 0;
+                try {
+                    val = Double.parseDouble(line);
+                }
+                catch(Exception e) {
+                    System.err.println("Value specified is invalid at line " + (x + y*res_base)+1);
+                    System.exit(0);
+
+                }
+                if(val<0 || val>255) {
+                    System.err.println("Invalid integer value for color");
+                    System.exit(0);
+                }
                 Color c = new Color(val/255, val/255, val/255, 1);
                 gc.setFill(c);
                 gc.fillRect(x, y,1,1);
@@ -58,6 +99,7 @@ public class RITViewer extends Application {
                 y++;
             }
         }
+
         g.getChildren().add(can);
 
         // Set up the scene and put the image inside
